c6d41c47a9ff04a8a3f4f470c5a3f42661236e45
Raviskill <raviskillqode@gmail.com>
2024-07-10 09:17:44 +0530

    ggg

---
+++ stack/binary_search.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+int binary(int arr[],int high,int low,int x)
+{
+    while(low<=high)
+    {
+        int mid=low+(high-low)/2;
+
+        if(arr[mid]==x)
+        {
+            return mid;
+        }
+        if(arr[mid]<x)
+        {
+            low=mid+1;
+        }
+        else{
+            high = mid-1;
+        }
+    }
+    return -1;
+
+
+};
+int main()
+{
+    int arr[8]={1,5,10,15,20,25,30,35};
+    int result=binary(arr,7,0,20);
+    if(result==-1)
+    {
+        printf("Data not found");
+    }
+    else{
+        printf("Element of mid is:%d",result);
+    }
+     return 0;
+}
